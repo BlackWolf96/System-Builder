8de12920ad4b0a106d47f820a5e9a718843a4c7c
BlackWolf96 <oskar.blackwolf@gmail.com>
2021-12-02 21:10:03 +0100

    update

---
+++ assets/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Hello
+  </div>
+</template>
+
+<script>
+export default {
+  el: '#app',
+  name: 'app',
+}
+</script>
+
+<style>
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('apps', './assets/js/index.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -54,6 +55,9 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
+    
+    // enable Vue
+    .enableVueLoader()
 
     // enables Sass/SCSS support
     //.enableSassLoader()
